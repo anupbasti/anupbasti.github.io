9fdc3f1bd57bed3f939017a04eadea6c2f0179fd
Basti <anup.basti@accenture.com>
2015-11-18 15:22:11 +0530

    added dashboard

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ $(function () {
 var $user = $('#username');
 var $pass = $('#password');
 var $guests = $('#guests');
+var $events = $('#events');
+
 
 $('#sub').on('click', function(){
  
@@ -30,6 +32,35 @@ $('#sub').on('click', function(){
 
 });
 
+
+/* to pre populate form data when click on Edit
+
+$('#event').on('click', function(){
+	echo('success');
+  
+ $.ajax({
+	
+	 
+    type: 'GET',
+	url: 'http://demo4707540.mockable.io/dash',
+	success: function(data){
+		//$('form').loadJSON(data);
+		echo('success');
+		
+	
+	},
+	
+	error: function(){
+		alert('Cannot Load Data');
+	}
+	
+ 
+ });
+
+});
+
+*/
+
 var guestTemplate = "" +
 "<tr>"+
 "<td></td>"+
@@ -37,6 +68,20 @@ var guestTemplate = "" +
 "<td>{{lname}}</td>"+
 "<td>{{phone}}</td>"+
 "<td>{{limit}}</td>"+
+"</tr>";
+
+var eventTemplate = "" +
+"<tr>"+
+"<td><a href=event.html id='edit'>Edit</a>/<a href=# data-id='{{id}}' class='remove'>Del</a></td>"+
+//"<td><button data-id='{{id}}' class='remove'>Edit</button><br/><br/><button data-id='{{id}}' class='remove'>Delete</button></td>"+
+"<td>{{event}}</td>"+
+"<td>{{dj}}</td>"+
+"<td>{{limit}}</td>"+
+"<td>{{fee}}</td>"+
+"<td>{{datetime}}</td>"+
+"<td>{{deadline}}</td>"+
+"<td>{{clubid}}</td>"+
+"<td><img src={{banner}} width=120px height=90px></td>"+
 "</tr>";
 
 
@@ -60,22 +105,40 @@ $.ajax({
 
 });
 
+function addEvents(event){
+	$events.append(Mustache.render(eventTemplate,event));
+}
 
-/*
+$.ajax({
+	type: 'GET',
+	url: 'http://demo4707540.mockable.io/dashboard',
+	success: function(events) {
+		$.each(events, function(i, event){
+			addEvents(event);
+		});
+	},
+	error: function(){
+		alert('Error Loading Page');
+	}
 
-$ord.delegate('.remove', 'click', function() {
-	var $li = $(this).closest('li');
+});
+
+
+
+
+$events.delegate('.remove', 'click', function() {
+	var $tr = $(this).closest('tr');
 	$.ajax({
 	type: 'DELETE',
 	url: 'http://demo4707540.mockable.io/del',
 	
 	success: function(){
-			$li.fadeOut(300, function() {
+			$tr.fadeOut(300, function() {
 			remove();
 			});
 	}
 	
 	});
 });
-*/
+
 });
